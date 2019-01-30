50d34b3e8a2a6d8ccf66f304f724014b7380aef8
Jose Luis Ojosnegros <jl.ojosnegros.manchon@gmail.com>
2019-01-30 19:35:11 +0100

    apuntes sobre lo que podria ir la practica

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,9 @@
 import models.Game
 import views.{GameView, CoordinateView, GestorIO}
 
+//1 poder jugar en modo demo ( 0 jugadores ) o normal ( 2 jugadores como ahora)) -- > traits creo
+//2 paralelizar el control de si hay tres en ralla para uno y otro.
+//3 Hacer esto con actores
 object Main {
 
   var game = new Game
